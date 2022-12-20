7d9142e08dc460a73f786348f8f2ec948be3dc81
GabrielGomesFer <gabriel.ferreira2002@hotmail.com>
2022-12-20 20:46:55 -0300

    feat: projects

---
+++ utils/projectsArray.js
@@ -0,0 +1,38 @@
+const projArray = [
+    {
+        name: "Trybe Futebol Clube",
+        text: `O projeto trata-se de um desafio para consolidar todo o
+        aprendizado até então em backend. Sendo um dos projetos mais
+        desafiadores da Trybe, tive que utilizar todos os
+        conceitos ensinados e praticados desde então - utilização de
+        HOFs, CRUD, Sequelize, manipulação do banco de dados, criação e
+        validação de tokens JWT para login/cadastro de usuários,
+        validação/criptografia de senhas com o BCrypt e muito mais. Com
+        isso, o projeto trata-se de um sistema de gerenciamento de
+        campeonato do Trybe Futebol Clube (vide nome do projeto). Nele,
+        é possível realizar login como um usuário comum ou como um
+        administrador; visualizar partidas em andamento e já
+        finalizadas; alterar o placar das partidas em andamento;
+        finalizar partidas em andamento; adicionar novas partidas e
+        visualizar o placar geral do campeonato.`,
+        projectImg: "https://user-images.githubusercontent.com/52508045/208774667-6d833be9-8990-4484-b817-6df1b9d31cc4.png"
+    },
+    {
+        name: "Delivery App: Unosso Bar",
+        text: `O projeto trata-se de um desafio em grupo, feito para consolidar todo o
+        aprendizadode back-end, front-end e da área de QA. Sendo o projeto mais
+        desafiador da Trybe, tivemos que utilizar todos os
+        conceitos ensinados e praticados desde então - utilização de
+        HOFs, CRUD, Sequelize, manipulação do banco de dados, criação e
+        validação de tokens JWT para login/cadastro de usuários,
+        validação/criptografia de senhas com o BCrypt, gerenciamento de estado,
+        estilizaçao, conexão back-front e muito mais. Com
+        isso, o projeto trata-se de um aplicativo de delivery especializado em bebidas. Nele,
+        é possível realizar login como um usuário comum (ou se registrar caso não tenha login), como um vendedor ou como um
+        administrador; adicionar e remover produtos do carrinho; validar vendas; marcar pedidos como entregues;
+        cadastrar novos produtos; cadastrar novos vendedores e administradores.`,
+        projectImg: "https://user-images.githubusercontent.com/52508045/208785599-f436c1a4-7ef0-4f35-8231-e5f90bffb0f1.jpg"
+    }
+]
+
+module.exports = { projArray }
